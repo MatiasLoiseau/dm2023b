0e26bc80c2640b243d20401774ca051e3b144d9d
MatiasLoiseau <matiasloiseau@gmail.com>
2023-09-28 11:03:58 -0300

    fix: path run workflow experimento 1

--- src/workflow-inicial_01/z601_RUN_correr_workflow_01.r
+++ src/workflow-inicial_01/z601_RUN_correr_workflow_01.r
@@ -9,10 +9,10 @@ options(error = function() {
 
 # corrida de cada paso del workflow
 correr_todo_junto <- function(){
-  source("~/dm2023b/src/workflow-inicial/z611_CA_reparar_dataset_01.r")
-  source("~/dm2023b/src/workflow-inicial/z621_DR_corregir_drifting_01.r")
-  source("~/dm2023b/src/workflow-inicial/z631_FE_historia_01.r")
-  source("~/dm2023b/src/workflow-inicial/z641_TS_training_strategy_01.r")
+  source("~/dm2023b/src/workflow-inicial_01/z611_CA_reparar_dataset_01.r")
+  source("~/dm2023b/src/workflow-inicial_01/z621_DR_corregir_drifting_01.r")
+  source("~/dm2023b/src/workflow-inicial_01/z631_FE_historia_01.r")
+  source("~/dm2023b/src/workflow-inicial_01/z641_TS_training_strategy_01.r")
 
   # ultimos pasos, muy lentos
   source("~/dm2023b/src/workflow-inicial/z651_HT_lightgbm_01.r")
